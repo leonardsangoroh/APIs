9b268e27391c9af712283b15849e3bf29230971c
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-02-13 13:23:29 +0300

    GitHub API
    Using the GitHub API

---
+++ GithubAPI.php
@@ -0,0 +1,34 @@
+<?php
+
+    $ch = curl_init();
+
+    $headers = [
+        "Authorization: token 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",
+        "User-Agent: leonardsangoroh"
+    ];
+
+    curl_setopt_array($ch, [
+        CURLOPT_URL => "https://api.github.com/user/starred/eddiemg10/Nyumbani",
+        CURLOPT_RETURNTRANSFER => true,
+        //Request headers
+       CURLOPT_HTTPHEADER => $headers
+       //Another user-agent option
+       //CURLOPT_USERAGENT => "leonardsangoroh"
+
+   ]);
+
+   
+
+    //Execute
+    $response = curl_exec($ch);
+
+    //Display status codes
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    
+    curl_close($ch);
+
+    echo $status_code. "\n";
+
+    echo $response. "\n";
+
+?>
